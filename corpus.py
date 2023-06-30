582492ea3f05b9944d85b74c4396a185083d7985
yoshi-pc <yoooo@yoshipc.net>
2023-06-30 14:45:32 +0900

    load imdb id map file

--- corpus.py
+++ corpus.py
@@ -1,10 +1,15 @@
-from typing import Union
+from typing import Union, List
 from pathlib import Path
+import json
 from .trec_jsonl_parser import TRECJsonlParser
 
 class Corpus(TRECJsonlParser):
-    def __init__(self, path: Union[Path, str], delimiter :str = ".", dtype: dict = {}) -> None:
+    def __init__(self, path: Union[Path, str], wikiid_to_imdbid :Union[Path, str], delimiter :str = ".", dtype: dict = {}) -> None:
         super().__init__(path, delimiter, dtype)
+        with open(wikiid_to_imdbid, "r") as fp:
+            self.__imdb_id_map = json.load(fp)
+        if len(self.__imdb_id_map) == 0:
+            raise ValueError("cannot load the IMDb id mapping.")
     
     def search_by_wikidata_id(self, id: str) -> int:
         if not id.startswith("Q"):
@@ -12,7 +17,9 @@ class Corpus(TRECJsonlParser):
         df = self.get_df()
         hit = df.query(f"wikidata_id == '{id}'").index.tolist()
         if len(hit) > 1:
-            raise ValueError("duplicated id was given")
+            raise ValueError("duplicated id was given.")
+        elif len(hit) == 0:
+            raise ValueError("unknown wikidata id was given.")
         return hit[0]
 
     def get_wikidata_classes_length(self, index: int) -> int:
@@ -30,6 +37,13 @@ class Corpus(TRECJsonlParser):
         if len(ret) > 1 or len(ret) == 0:
             raise ValueError(f"id is not unique. (length: {len(ret)})")
         return ret[0]
+
+    def get_imdb_id(self, index: int, url: bool = False) -> List[str]:
+        ret = self.__imdb_id_map.get(self.get_info(index, "doc_id"), [])
+        if url:
+            return [f"https://www.imdb.com/title/{item}/" for item in ret]
+        else:
+            return ret
     
     @staticmethod
     def help() -> str:
